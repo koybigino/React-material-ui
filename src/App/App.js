de84221eb8b7b0030b5d9fc4c4af4423b5c560da
BIKOY-ELIE <koybigino@gmail.com>
2022-02-21 12:52:34 +0100

    React With Material UI Version 1.0

---
+++ src/App/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import SideMenu from '../components/SideMenu'
+import Header from '../components/Header'
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  },
+})
+
+function App() {
+  const classes = useStyles()
+  return (
+    <>
+      <SideMenu/>
+      <div className={ classes.appMain }>
+        <Header></Header>
+      </div>
+    </>
+  );
+}
+
+export default App;
