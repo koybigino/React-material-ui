62ba1be18e2edebf6eeb2cdabc7afe9efc9b6f6e
BIKOY-ELIE <koybigino@gmail.com>
2022-02-23 12:30:21 +0100

    background and Theme de 30:00 min à 52:00

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import { makeStyles, CssBaseline } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@mui/material';
 import SideMenu from '../components/SideMenu'
 import Header from '../components/Header'
-import { makeStyles, CssBaseline } from '@material-ui/core';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff0000",
+      light: "#3c44b126",
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526",
+    },
+    backgroundColor:{
+      default: "#f4f5fd"
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain: {
@@ -13,13 +32,13 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles()
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu/>
       <div className={ classes.appMain }>
         <Header></Header>
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
